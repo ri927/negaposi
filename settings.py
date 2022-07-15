c35d09064f8e860d3299eab8590491aed3972d07
ri927 <InfoStu1289@gmail.com>
2022-07-15 22:38:25 +0900

    envの追加

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), 'env/.env')
 load_dotenv(dotenv_path)
 
 BEARER_TOKEN = os.environ.get('BEARER_TOKEN')
